cda76e22605ba536cce7f7beb563c64cf1d24bf7
mheydon1973 <mheydon1973>
2006-05-23 21:40:02 +0000

    fixes a warning about TOKs and things I think, as reported by tuck3r on IRC

--- functions.inc.php
+++ functions.inc.php
@@ -49,7 +49,7 @@ function findmefollow_get_config($engine) {
                                                 $ext->add($contextname, $grpnum, '', new ext_setvar("_ALERT_INFO", $dring));
                                         }
 					// check for old prefix
-					$ext->add($contextname, $grpnum, '', new ext_gotoif('$[${CALLERID(name):0:${LEN(${RGPREFIX})}} != ${RGPREFIX}]', 'NEWPREFIX'));
+					$ext->add($contextname, $grpnum, '', new ext_gotoif('$["${CALLERID(name):0:${LEN(${RGPREFIX})}}" != "${RGPREFIX}]"', 'NEWPREFIX'));
 					// strip off old prefix
 					$ext->add($contextname, $grpnum, '', new ext_setvar('CALLERID(name)','${CALLERID(name):${LEN(${RGPREFIX})}}'));
 					// set new prefix
@@ -63,7 +63,7 @@ function findmefollow_get_config($engine) {
 					$ext->add($contextname, $grpnum, '', new ext_setvar('RingGroupMethod',$strategy));
 					if ((isset($annmsg) ? $annmsg : '') != '') {
 						// should always answer before playing anything, shouldn't we ?
-						$ext->add($contextname, $grpnum, '', new ext_gotoif('$[${DIALSTATUS} = ANSWER]','DIALGRP'));			
+						$ext->add($contextname, $grpnum, '', new ext_gotoif('$["${DIALSTATUS}" = "ANSWER"]','DIALGRP'));			
 						$ext->add($contextname, $grpnum, '', new ext_answer(''));
 						$ext->add($contextname, $grpnum, '', new ext_wait(1));
 						$ext->add($contextname, $grpnum, '', new ext_playback($annmsg));
