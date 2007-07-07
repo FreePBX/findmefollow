afac8b9483c933c5e7a5f247097fe4fe4d58d00e
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2007-07-07 15:11:18 +0000

    #2057 don't strip CID prefix if no prefix is being added

--- functions.inc.php
+++ functions.inc.php
@@ -92,13 +92,14 @@ function findmefollow_get_config($engine) {
 					$ext->add($contextname, $grpnum, 'skipvmblk', new ext_setvar('__NODEST', '${EXTEN}'));
 
 					// deal with group CID prefix
-					$ext->add($contextname, $grpnum, '', new ext_gotoif('$["foo${RGPREFIX}" = "foo"]', 'REPCID'));
-					$ext->add($contextname, $grpnum, '', new ext_gotoif('$["${RGPREFIX}" != "${CALLERID(name):0:${LEN(${RGPREFIX})}}"]', 'REPCID'));
-					$ext->add($contextname, $grpnum, '', new ext_noop('Current RGPREFIX is ${RGPREFIX}....stripping from Caller ID'));
-					$ext->add($contextname, $grpnum, '', new ext_setvar('CALLERID(name)', '${CALLERID(name):${LEN(${RGPREFIX})}}'));
-					$ext->add($contextname, $grpnum, '', new ext_setvar('_RGPREFIX', ''));
-					$ext->add($contextname, $grpnum, 'REPCID', new ext_noop('CALLERID(name) is ${CALLERID(name)}'));
+					// but strip only if you plan on setting a new one
 					if ($grppre != '') {
+						$ext->add($contextname, $grpnum, '', new ext_gotoif('$["foo${RGPREFIX}" = "foo"]', 'REPCID'));
+						$ext->add($contextname, $grpnum, '', new ext_gotoif('$["${RGPREFIX}" != "${CALLERID(name):0:${LEN(${RGPREFIX})}}"]', 'REPCID'));
+						$ext->add($contextname, $grpnum, '', new ext_noop('Current RGPREFIX is ${RGPREFIX}....stripping from Caller ID'));
+						$ext->add($contextname, $grpnum, '', new ext_setvar('CALLERID(name)', '${CALLERID(name):${LEN(${RGPREFIX})}}'));
+						$ext->add($contextname, $grpnum, '', new ext_setvar('_RGPREFIX', ''));
+						$ext->add($contextname, $grpnum, 'REPCID', new ext_noop('CALLERID(name) is ${CALLERID(name)}'));
 						$ext->add($contextname, $grpnum, '', new ext_setvar('_RGPREFIX', $grppre));
 						$ext->add($contextname, $grpnum, '', new ext_setvar('CALLERID(name)','${RGPREFIX}${CALLERID(name)}'));
 					}
