52966f36cd65ade56f71b9a248c7412c35f9216d
Bryan Walters <secretop@gmail.com>
2013-10-22 14:55:05 +0000

    FREEPBX-6855 allow + in followme list

--- page.findmefollow.php
+++ page.findmefollow.php
@@ -60,7 +60,7 @@ if (isset($_REQUEST["grplist"])) {
 		$grplist[$key] = trim($grplist[$key]);
 		
 		// remove invalid chars
-		$grplist[$key] = preg_replace("/[^0-9#*]/", "", $grplist[$key]);
+		$grplist[$key] = preg_replace("/[^0-9#*+]/", "", $grplist[$key]);
 
 		if ($grplist[$key] == ltrim($extdisplay,'GRP-').'#')
 			$grplist[$key] = rtrim($grplist[$key],'#');
