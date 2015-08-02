ed851cdd52af312ade6d122f3366a8f9a48ab30d
Rob Thomas <xrobau@gmail.com>
2015-08-02 15:08:50 -0700

    Remove unneeded divs

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,20 +1,16 @@
 <div class="element-container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-3">
-						<label class="control-label" for="fmfm"><?php echo _('Enable Find Me/Follow Me')?></label>
-					</div>
-					<div class="col-md-9">
-						<span class="radioset">
-							<input type="radio" name="fmfm" id="fmfm_on" value="yes" <?php echo ($fmfm) ? 'checked' : ''?>>
-							<label for="fmfm_on"><?php echo _('Yes')?></label>
-							<input type="radio" name="fmfm" id="fmfm_off" value="no" <?php echo ($fmfm) ? '' : 'checked'?>>
-							<label for="fmfm_off"><?php echo _('No')?></label>
-						</span>
-					</div>
-				</div>
+		<div class="form-group">
+			<div class="col-md-3">
+				<label class="control-label" for="fmfm"><?php echo _('Enable Find Me/Follow Me')?></label>
+			</div>
+			<div class="col-md-9">
+				<span class="radioset">
+					<input type="radio" name="fmfm" id="fmfm_on" value="yes" <?php echo ($fmfm) ? 'checked' : ''?>>
+					<label for="fmfm_on"><?php echo _('Yes')?></label>
+					<input type="radio" name="fmfm" id="fmfm_off" value="no" <?php echo ($fmfm) ? '' : 'checked'?>>
+					<label for="fmfm_off"><?php echo _('No')?></label>
+				</span>
 			</div>
 		</div>
 	</div>
