9348f65c39dc8773a0855eb51e85607109eadcd4
James Finstrom <jfinstrom@sangoma.com>
2015-11-02 15:03:53 -0700

    menu adjustments

--- views/bootnav.php
+++ views/bootnav.php
@@ -10,7 +10,7 @@ if (isset($amp_conf["AMPEXTENSIONS"]) && ($amp_conf["AMPEXTENSIONS"] == "devicea
 ?>
 <div id="toolbar-fmfm">
 <a href="config.php?display=findmefollow" class="btn btn-default <?php echo ($request['view'] == ''? 'hidden':'')?>"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Followme Groups") ?></a>
-<a href="<?php echo $editURL?>" class="btn btn-default"><i class="fa fa-edit"></i>&nbsp; <?php echo $EXTorUSER . " " . ltrim($request['extdisplay'],'GRP-') ?></a>
+<a href="<?php echo $editURL?>" class="btn btn-default"><i class="fa fa-external-link"></i>&nbsp; <?php echo $EXTorUSER . " " . ltrim($request['extdisplay'],'GRP-') ?></a>
 </div>
 <table data-url="ajax.php?module=findmefollow&amp;command=getJSON&amp;jdata=grid" data-cache="false" data-toggle="table" data-search="true" class="table" data-toolbar="#toolbar-fmfm" id="table-all-side">
     <thead>
