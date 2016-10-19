03fbe4c9305476d061bf25e9f9162276f77f795d
Andrew Nagy <andrew.nagy@the159.com>
2016-10-19 21:52:23 +0000

    setup new widget for functionality

--- ucp/assets/js/global.js
+++ ucp/assets/js/global.js
@@ -1,30 +1,40 @@
 var FindmefollowC = UCPMC.extend({
 	init: function(){
 	},
+	/*
 	display: function() {
+		var $this = this;
 		$('#module-page-findmefollow input[type="text"], #module-page-findmefollow textarea').change(function() {
 			$(this).blur(function() {
-				Findmefollow.saveSettings({key: $(this).prop('name'), value: $(this).val()});
+				$this.saveSettings({key: $(this).prop('name'), value: $(this).val()});
 				$(this).off('blur');
 			});
 		});
 		$('#module-page-findmefollow input[type="checkbox"]').change(function() {
-			Findmefollow.saveSettings({key: $(this).prop('name'), value: $(this).is(':checked')});
+			$this.saveSettings({key: $(this).prop('name'), value: $(this).is(':checked')});
 		});
 		$('#module-page-findmefollow select').change(function() {
-			Findmefollow.saveSettings({key: $(this).prop('name'), value: $(this).val()});
+			$this.saveSettings({key: $(this).prop('name'), value: $(this).val()});
 		});
+	},
+	*/
+	displayWidget: function(widget_id) {
+		console.log("I [findmefollowme] was told to display for ["+widget_id+"]!");
+		$("#ddial").change(function() {
+			console.log("thingy changed!");
+		});
+	},
+	displayWidgetSettings: function(widget_id) {
 	},
 	saveSettings: function(data) {
 		data.ext = extension;
 		$.post( "index.php?quietmode=1&module=findmefollow&command=settings", data, function( data ) {
 			$('#module-page-findmefollow .message').text(data.message).addClass('alert-'+data.alert).fadeIn('fast', function() {
 				$(this).delay(5000).fadeOut('fast', function() {
-					$('.masonry-container').packery();
+					//$('.masonry-container').packery();
 				});
 			});
-			$('.masonry-container').packery();
+			//$('.masonry-container').packery();
 		});
 	}
 });
-var Findmefollow = new FindmefollowC();
