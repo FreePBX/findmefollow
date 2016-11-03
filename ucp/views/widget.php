b944909b906cdd9caa81c2c4d7c6a649bbfe67ea
James Finstrom <jfinstrom@sangoma.com>
2016-11-02 18:22:52 -0700

    rework widget with bootstrap toggle

--- ucp/views/widget.php
+++ ucp/views/widget.php
@@ -2,16 +2,14 @@
 extension = <?php echo $extension; ?>;
 </script>
 <div class="message alert" style="display:none;"></div>
-<form role="form">
-	<div class="form-group">
-		<label for="ddial-h" class="help"><?php echo _('Enable')?> <i class="fa fa-question-circle"></i></label>
-		<div class="onoffswitch">
-			<input type="checkbox" name="ddial" class="onoffswitch-checkbox" id="ddial" <?php echo ($enabled) ? 'checked' : ''?>>
-			<label class="onoffswitch-label" for="ddial">
-				<div class="onoffswitch-inner"></div>
-				<div class="onoffswitch-switch"></div>
-			</label>
+<form role="form" class="form" >
+		<div class="form-group">
+			<div class="col-xs-3">
+				<label for="ddial-h" class="help"><?php echo _('Follow Me')?></label>
+			</div>
+			<div class="col-xs-9">
+				<input type="checkbox" name="ddial" data-toggle="toggle" id="ddial" data-on="<?php echo _('Enabled')?>" data-off="<?php echo _('Disabled')?>" <?php echo ($enabled) ? 'checked' : ''?>>
+			</div>
 		</div>
-		<span class="help-block help-hidden" data-for="ddial-h"><?php echo _('When enabled any call to this extension will go to this Follow-Me instead, including directory calls by name from IVRs. If disabled, calls will go only to the extension.')?></span>
 	</div>
 </form>
