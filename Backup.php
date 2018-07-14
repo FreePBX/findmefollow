21c52da6fd16256f14715c8b2aca09e8326463ac
James Finstrom <jfinstrom@sangoma.com>
2018-07-14 01:39:28 +0000

    Restore cleanup

--- Backup.php
+++ Backup.php
@@ -1,11 +1,10 @@
 <?php
-namespace FreePBX\modules\__MODULENAME__;
+namespace FreePBX\modules\Findmefollow;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
   public function runBackup($id,$transaction){
-        $configs = $this->FreePBX->Findmefollow->bulkhandlerExport('extensions');
-        $this->addConfigs($configs);
-    }
+    $configs = $this->FreePBX->Findmefollow->bulkhandlerExport('extensions');
+    $this->addConfigs($configs);
     $this->addDependency('core');
     $this->addDependency('userman');
     $this->addDependency('recordings');
--- Restore.php
+++ Restore.php
@@ -1,8 +1,9 @@
 <?php
-namespace FreePBX\modules\__MODULENAME__;
+namespace FreePBX\modules\Findmefollow;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
+	dbug("HERE");
         $configs = $this->getConfigs();
         $this->FreePBX->Findmefollow->bulkhandlerImport('extensions', $configs);
   }
