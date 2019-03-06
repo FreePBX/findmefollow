561a71a707d114aefe7fbcc68e3978169c24cc3b
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 16:55:52 -0500

    [Module Tag script: findmefollow 15.0.11]

--- Backup.php
+++ Backup.php
@@ -2,12 +2,13 @@
 namespace FreePBX\modules\Findmefollow;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $configs = $this->FreePBX->Findmefollow->bulkhandlerExport('extensions');
-    $this->addConfigs($configs);
-    $this->addDependency('core');
-    $this->addDependency('userman');
-    $this->addDependency('recordings');
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$this->addDependency('core');
+		$this->addDependency('userman');
+		$this->addDependency('recordings');
+		$this->addConfigs([
+			'data' => $this->FreePBX->Findmefollow->bulkhandlerExport('extensions'),
+			'features' => $this->dumpFeatureCodes()
+		]);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -3,11 +3,13 @@ namespace FreePBX\modules\Findmefollow;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
 	public function runRestore($jobid){
-				$configs = $this->getConfigs();
-				$this->FreePBX->Findmefollow->bulkhandlerImport('extensions', $configs);
+		$configs = $this->getConfigs();
+		$this->FreePBX->Findmefollow->bulkhandlerImport('extensions', $configs['data']);
+		$this->importFeatureCodes($configs['features']);
 	}
 
 	public function processLegacy($pdo, $data, $tables, $unknownTables){
 		$this->restoreLegacyDatabase($pdo);
+		$this->restoreLegacyFeatureCodes($pdo);
 	}
 }
