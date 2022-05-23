a6a59c7e5bf541ddcf82d71cf9ceb231cfb2cc07
kapil gupta <kgupta@sangoma.com>
2022-05-23 07:36:01 +0000

    Revert "FREEI-4793 Fixed styling issues in GUI"
    This reverts commit 996f3b3fe1a9f065040d8d3a849d173983ce421b.

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,6 +1,6 @@
 <div class="element-container">
-	<div class="">
-		<div class="row form-group">
+	<div class="row">
+		<div class="form-group">
 			<div class="col-md-3">
 				<label class="control-label" for="fmfm"><?php echo _('Enable Find Me/Follow Me')?></label>
 			</div>
